b9137385b9b6d13085de1444f4fcba239e28c18a
oxmc <67136658+oxmc@users.noreply.github.com>
2021-12-18 03:50:52 -0800

    improve fetch response times

--- api.js
+++ api.js
@@ -9,13 +9,19 @@ if (!faviconjs) {
   };
 };
 
+/* Variables */
+var fav;
+
 /* Main */
 function GetFav(hash) {
-  fetch(`http://memefileserver.ml/jrp/favicon.php?hash=${hash}`).then(function(response) {
-    return response.json();
-  }).then(function(data) {
-    favicon(data.database64);
-  }).catch(function(err) {
-    console.log("error: "+err)
-  });
+  fetch(`http://memefileserver.ml/jrp/favicon.php?hash=${hash}`)
+    .then(res => res.json())
+    .then(data => fav = data.image);
+};
+
+async function SetFav(hash) {
+  fetch(`http://memefileserver.ml/jrp/favicon.php?hash=${hash}`)
+    .then(res => res.json())
+    .then(data => fav = data.image)
+    .then(() => favicon(data.image, "base64"));
 };
