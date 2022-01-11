fd9cc14b369dcbb3424ea01015abdb62584e4071
oxmc <67136658+oxmc@users.noreply.github.com>
2022-01-10 17:29:01 -0800

    Update api.js

--- api.js
+++ api.js
@@ -1,3 +1,6 @@
+// jrp.best fileserver api, copyright 2022-2023 (c) by oxmc and Givinghawk
+// Distributed under an MIT license: https://jrpbest-files.givinghawk.repl.co/LICENSE
+
 /* Include Favicon.js by oxmc */
 var faviconjs = document.querySelector("script[src~='https://fjs.oxmc.xyz/favicon.min.js']");
 if (!faviconjs) {
@@ -9,19 +12,45 @@ if (!faviconjs) {
   };
 };
 
-/* Variables */
-var fav;
-
-/* Main */
-function GetFav(hash) {
-  fetch(`http://memefileserver.ml/jrp/favicon.php?hash=${hash}`)
-    .then(res => res.json())
-    .then(data => fav = data.image);
+/* Include axios for get requests */
+if (!favicon()) {
+  // axios is not loaded
+  throw new Error("favicon.js is not loaded, please include it above this api script.");
 };
 
-async function SetFav(hash) {
-  fetch(`http://memefileserver.ml/jrp/favicon.php?hash=${hash}`)
-    .then(res => res.json())
-    .then(data => fav = data.image)
-    .then(() => favicon(data.image, "base64"));
+/* Check if async/await is availible */
+let isAsync = true;
+try {
+  eval('async () => {}');
+} catch (e) {
+  if (e instanceof SyntaxError)
+    isAsync = false;
+  else
+    throw new Error("Unable to use async/await, unable to use api");
+    /* throw e; // throws CSP error */
+}
+
+/* Events */
+var JrpRespEvent = new CustomEvent("JrpResponse", { "detail": "Response recived from JrpBest Servers" });
+
+/* Main Functions */
+async function GetB64File(accesskey, file) {
+  const res = await axios.get(`https://jrpbest-files.givinghawk.repl.co/api/v1/api.php?accesskey=${accesskey}&file=${file}`);
+  //console.log(res.data.file);
+  document.dispatchEvent(JrpRespEvent);
+  var resp = res.data;
+  return resp;
+};
+
+async function GetFav(hash) {
+  const res = await axios.get(`http://memefileserver.ml/jrp/favicon.php?hash=${hash}`);
+  document.dispatchEvent(JrpRespEvent);
+  var resp = res.data.image;
+  return resp;
+};
+
+async function setFav(hash) {
+  const res = await axios.get(`http://memefileserver.ml/jrp/favicon.php?hash=${hash}`);
+  document.dispatchEvent(JrpRespEvent);
+  favicon(res.data.image, "base64")
 };
